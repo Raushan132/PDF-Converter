f02c53bb2e81edf60cacc466b9ffe693a5b98564
Raushan132 <raushankumar132abc@gmail.com>
2025-05-12 14:14:03 +0530

    update in marge file

--- src/utils/BaseUrl.js
+++ src/utils/BaseUrl.js
@@ -1,2 +1,2 @@
-// export const baseUrl="https://onlinepdf.in:9002"
-export const baseUrl="http://localhost:9001"
+export const baseUrl="https://onlinepdf.in:9002"
+// export const baseUrl="http://localhost:9001"
